29d3b4ab0d12cc59430339fb75774ddea21d3562
nidunyan <dunyang.ni@mail.utoronto.ca>
2025-04-09 23:55:52 -0400

    'host'

--- A3/backend/index.js
+++ A3/backend/index.js
@@ -1,22 +1,6 @@
 #!/usr/bin/env node
 'use strict'
-
-const port = (() => {
-  const args = process.argv
-
-  if (args.length !== 3) {
-    console.error('usage: node index.js port')
-    process.exit(1)
-  }
-
-  const num = parseInt(args[2], 10)
-  if (isNaN(num)) {
-    console.error('error: argument must be an integer.')
-    process.exit(1)
-  }
-
-  return num
-})()
+const PORT = process.env.PORT || 3000;
 
 const express = require('express')
 const app = express()
@@ -45,8 +29,8 @@ app.use('/transactions', transactionRouter)
 app.use('/events', eventsRouter)
 app.use('/promotions', promotionsRouter)
 
-const server = app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
 
 server.on('error', (err) => {
